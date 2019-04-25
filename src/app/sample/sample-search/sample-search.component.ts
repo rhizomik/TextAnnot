653c262408ddd594421ea1eae93c29d5df3c9cb6
Albert Berga <albertbergagatius@gmail.com>
2019-04-25 16:03:10 +0200

    Samples filtered list - extract fragments through regex (firefox support)

--- src/app/sample/sample-search/sample-search.component.ts
+++ src/app/sample/sample-search/sample-search.component.ts
@@ -38,13 +38,14 @@ export class SampleSearchComponent {
 
   private getTextFragments(searchTerm: string, text: string): TextFragment[] {
     const result = [];
-    const regex = new RegExp(`(?<=(.{60}))(\\b${searchTerm}\\b)(?=(.{0,60}))`, 'gis');
-    const auxText = `${'.'.repeat(59)} ${text}`;
-    let match = regex.exec(auxText);
+    // const regex = new RegExp(`(?<=(.{60}))(\\b${searchTerm}\\b)(?=(.{0,60}))`, 'gi'); awaiting for lookbehind firefox support
+    // const auxText = `${'.'.repeat(59)} ${text}`;
+    const regex = new RegExp(`(.*)(\\b${searchTerm}\\b)(?=(.{0,60}))`, 'gi');
+    let match = regex.exec(text);
     while (match != null) {
-      result.push(new TextFragment(match[1].substring(match[1].indexOf(' ') + 1),
+      result.push(new TextFragment(text.substring(match[1].length < 60 ? 0 : text.indexOf(' ', match[1].length - 60) + 1, match[1].length),
         match[2], match[3].length < 60 ? match[3] : match[3].substring(0, match[3].lastIndexOf(' '))));
-      match = regex.exec(auxText);
+      match = regex.exec(text);
     }
     return result;
   }
