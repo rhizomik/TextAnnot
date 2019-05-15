a674d8cc68e66533ab38fad728ede24987650295
bergacat1 <albertbergagatius@gmail.com>
2019-05-15 13:04:56 +0200

    Samples list fragments fix

--- src/app/sample/sample.service.ts
+++ src/app/sample/sample.service.ts
@@ -30,11 +30,11 @@ export class SampleService extends RestService<Sample> {
     const result = [];
     // const regex = new RegExp(`(?<=(.{60}))(\\b${searchTerm}\\b)(?=(.{0,60}))`, 'gi'); awaiting for lookbehind firefox support
     // const auxText = `${'.'.repeat(59)} ${text}`;
-    const regex = new RegExp(`(.*)(\\b${searchTerm}\\b)(?=(.{0,60}))`, 'gi');
+    const regex = new RegExp(`(\\b${searchTerm}\\b)`, 'gi');
     let match = regex.exec(text);
     while (match != null) {
-      result.push(new TextFragment(text.substring(match[1].length < 60 ? 0 : text.indexOf(' ', match[1].length - 60) + 1, match[1].length),
-        match[2], match[3].length < 60 ? match[3] : match[3].substring(0, match[3].lastIndexOf(' '))));
+      result.push(new TextFragment(text.substring(match.index < 60 ? 0 : text.indexOf(' ', match.index - 60) + 1, match.index),
+        match[0], text.length - match.index < 60 ? text.substring(match.index + match[0].length) : text.substring(match.index + match[0].length, text.indexOf(' ', match.index + match[0].length + 55))));
       match = regex.exec(text);
     }
     return result;
