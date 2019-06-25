0fc9bd6c3a07af1fd55a647f7c4c735318f5cb19
bergacat1 <albertbergagatius@gmail.com>
2019-06-25 10:06:06 +0200

    Sample filters - statistics relative frequencies

--- src/app/shared/models/sample-statistics.ts
+++ src/app/shared/models/sample-statistics.ts
@@ -1,36 +1,45 @@
 export class ValueStatistic {
   value: string;
-  statistic: number;
+  absoluteFreq: number;
+  relativeFreq: number;
+  globalRelativeFreq: number;
 
-  constructor(value: string, statistic: number) {
+  constructor(value: string, absoluteFreq: number, relativeFreq: number, globalRelativeFreq: number) {
     this.value = value;
-    this.statistic = statistic;
+    this.absoluteFreq = absoluteFreq;
+    this.relativeFreq = relativeFreq;
+    this.globalRelativeFreq = globalRelativeFreq;
   }
 }
 
 export class MetadataStatistics {
   metadataField: string;
   statistics: ValueStatistic[];
-  constructor(field: string, statistics: Object) {
+  constructor(field: string, statistics: Object, globalStatistics: Object, samplesCount: number, totalSamplesCount: number) {
     this.metadataField = field;
     this.statistics = [];
     for (const statistic in statistics) {
-      this.statistics.push(new ValueStatistic(statistic, statistics[statistic]));
+      const relativeFreq = Math.round(1000 * statistics[statistic] / samplesCount) / 1000;
+      const globalRelativeFreq = Math.round(1000 * globalStatistics[statistic] / totalSamplesCount) / 1000;
+      this.statistics.push(new ValueStatistic(statistic, statistics[statistic],
+         relativeFreq, globalRelativeFreq));
     }
-    this.statistics.sort((a, b) => b.statistic - a.statistic);
+    this.statistics.sort((a, b) => b.absoluteFreq - a.absoluteFreq);
   }
 }
 
 export class SampleStatistics {
   occurrences: number;
   samples: number;
+  totalSamples: number;
   metadataStatistics: MetadataStatistics[];
+  globalMetadataStatistics: MetadataStatistics[];
 
   constructor(data: Object) {
     Object.assign(this, data);
     this.metadataStatistics = [];
     for (const metadataStatistic in data['metadataStatistics']) {
-      this.metadataStatistics.push(new MetadataStatistics(metadataStatistic, data['metadataStatistics'][metadataStatistic]));
+      this.metadataStatistics.push(new MetadataStatistics(metadataStatistic, data['metadataStatistics'][metadataStatistic], data['globalMetadataStatistics'][metadataStatistic], this.samples, this.totalSamples));
     }
   }
 }
