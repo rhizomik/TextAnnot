81745893a276c39d19ec4368542617e1d9e9094a
bergacat1 <albertbergagatius@gmail.com>
2019-07-01 12:42:37 +0200

    MetadataField type selector

--- src/app/error-handler/error-message.service.ts
+++ src/app/error-handler/error-message.service.ts
@@ -8,10 +8,22 @@ export class ErrorMessageService {
 
   errorMessage$ = this.errorMessageSource.asObservable();
 
+  disabled = false;
+
   constructor() {
   }
 
   showErrorMessage(errorMessage: string) {
-    this.errorMessageSource.next(errorMessage);
+    if (!this.disabled) {
+      this.errorMessageSource.next(errorMessage);
+    }
+  }
+
+  disableErrorsHandler() {
+    this.disabled = true;
+  }
+
+  enableErrorsHandler() {
+    this.disabled = false;
   }
 }
