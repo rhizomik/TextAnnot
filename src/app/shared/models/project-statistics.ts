df897ad727482a8785a7c2b17181dba69a7ed8ca
bergacat1 <albertbergagatius@gmail.com>
2019-08-05 12:45:12 +0200

    Corpus metadata statistics

--- src/app/shared/models/project-statistics.ts
+++ src/app/shared/models/project-statistics.ts
@@ -1,10 +1,38 @@
+export class ProjectValueStatistic {
+  value: string;
+  absoluteFreq: number;
+
+  constructor(value: string, absoluteFreq: number) {
+    this.value = value;
+    this.absoluteFreq = absoluteFreq;
+  }
+}
+
+export class ProjectMetadataStatistics {
+  metadataField: string;
+  statistics: ProjectValueStatistic[];
+  constructor(field: string, statistics: Object) {
+    this.metadataField = field;
+    this.statistics = [];
+    for (const statistic in statistics) {
+      this.statistics.push(new ProjectValueStatistic(statistic, statistics[statistic]));
+    }
+    this.statistics.sort((a, b) => b.absoluteFreq - a.absoluteFreq);
+  }
+}
+
 export class ProjectStatistics {
   totalSamples: number;
   totalWords: number;
   annotatedSamples: number;
   totalAnnotations: number;
+  metadataStatistics: ProjectMetadataStatistics[];
 
   constructor(data: Object) {
     Object.assign(this, data);
+    this.metadataStatistics = [];
+    for (const metadataStatistic in data['metadataStatistics']) {
+      this.metadataStatistics.push(new ProjectMetadataStatistics(metadataStatistic, data['metadataStatistics'][metadataStatistic]));
+    }
   }
 }
--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {Project} from "../shared/models/project";
-import {ProjectService} from "../core/services/project.service";
-import {SampleService} from "../core/services/sample.service";
-import {SampleStatistics} from "../shared/models/sample-statistics";
-import {ProjectStatistics} from "../shared/models/project-statistics";
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Project} from '../shared/models/project';
+import {ProjectService} from '../core/services/project.service';
+import {ProjectMetadataStatistics, ProjectStatistics} from '../shared/models/project-statistics';
+import {AuthenticationBasicService} from '../login-basic/authentication-basic.service';
+import {MetadataStatistics} from '../shared/models/sample-statistics';
+import {ExportToCsv} from 'export-to-csv';
+import {MetadataFieldService} from '../core/services/metadata-field.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-statistics',
@@ -15,14 +18,45 @@ export class StatisticsComponent implements OnInit {
   private project: Project;
   public statistics: ProjectStatistics;
 
+  @ViewChild('statistic') private statistic: ElementRef;
+  @ViewChild('count') private count: ElementRef;
+
   constructor(
     private projectService: ProjectService,
+    private authentication: AuthenticationBasicService,
+    private metadataFieldService: MetadataFieldService,
+    private router: Router
   ) { }
 
   async ngOnInit() {
     this.project = await this.projectService.getProject();
     this.projectService.getProjectStatistics(this.project)
       .subscribe(statistics => this.statistics = statistics);
+  }
+
+  navigateFieldValues(metadataFieldName: string) {
+    this.metadataFieldService.getMetadataFieldsByName(metadataFieldName)
+      .subscribe(value => {
+        this.router.navigate(['metadataFields', value.id, 'values']);
+      });
+  }
+
+  downloadStatistics(fieldStatistics: ProjectMetadataStatistics) {
+    const options = {
+      fieldSeparator: ';',
+      filename: 'export',
+      quoteStrings: '"',
+      decimalSeparator: '.',
+      showLabels: true,
+      showTitle: true,
+      title: fieldStatistics.metadataField,
+      useTextFile: false,
+      useBom: true,
+      useKeysAsHeaders: false,
+      headers: [this.statistic.nativeElement.textContent, this.count.nativeElement.textContent]
+    };
+    const exporter = new ExportToCsv(options);
+    exporter.generateCsv(fieldStatistics.statistics);
   }
 
 }
