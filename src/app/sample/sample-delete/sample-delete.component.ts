c610667e2ddda437ee30311d77fd80fdeee8f431
Roberto García <roberto@rhizomik.net>
2019-12-17 01:01:36 +0100

    Fix sample delete, remove first all metadata values

--- src/app/sample/sample-delete/sample-delete.component.ts
+++ src/app/sample/sample-delete/sample-delete.component.ts
@@ -1,7 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {Sample} from '../../shared/models/sample';
-import {SampleService} from '../../core/services/sample.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Sample } from '../../shared/models/sample';
+import { SampleService } from '../../core/services/sample.service';
+import { MetadataValue } from '../../shared/models/metadataValue';
+import { MetadataValueService } from '../../core/services/metadataValue.service';
+import { flatMap } from 'rxjs/operators';
+import { forkJoin } from 'rxjs/internal/observable/forkJoin';
 
 @Component({
   selector: 'app-sample-delete',
@@ -9,21 +13,26 @@ import {SampleService} from '../../core/services/sample.service';
 })
 export class SampleDeleteComponent implements OnInit {
   public mSample: Sample = new Sample();
+  public metadataValues: MetadataValue[] = [];
   public errorMessage: string;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
-              private sampleService: SampleService) {
+              private sampleService: SampleService,
+              private metadataValueService: MetadataValueService) {
   }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
-    this.sampleService.get(id).subscribe(
-      msample => this.mSample = msample);
+    this.sampleService.get(id).subscribe(sample => {
+      this.mSample = sample;
+    });
   }
 
   delete() {
-    this.sampleService.delete(this.mSample).subscribe(
-      () => this.router.navigate(['samples']));
+    this.metadataValueService.findByForA(this.mSample).pipe(
+      flatMap((values: MetadataValue[]) => forkJoin(values.map(value => this.metadataValueService.delete(value)))),
+      flatMap( () => this.sampleService.delete(this.mSample))
+    ).subscribe(() => this.router.navigate(['samples']));
   }
 }
